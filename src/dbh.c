e710f3b9c42ae9ad089dabfb50f9b8c51940e124
agnitio247 <agnitio247@protonmail.com>
2019-04-08 22:55:24 -0400

    done print functions

--- src/dbh.c
+++ src/dbh.c
@@ -8,7 +8,7 @@
 const char charTable[] = "0123456789abcdefghijklmnopqrstuvwxyz";
 
 // convert dec to every base from 2 to 36
-const char* ConvertDec(double decimalNumber, char** ptr, int base, int* length) {
+const char* ConvertDec(int decimalNumber, char** ptr, int base, int* length) {
   char* output = NULL; // doesn't work if not specifed NULL for some fucking stupid reason
   int sizeOfOutput = 0; // initialize as 0 to check later
   int power;
@@ -16,6 +16,7 @@ const char* ConvertDec(double decimalNumber, char** ptr, int base, int* length) 
   if (base > 36 || base < 2) { // max and min base
     output = malloc(11);
     strcpy(output, "BASE_ERROR\0");
+    *ptr = output;
     return output;
   }
   if (decimalNumber < 0) { // cant deal with negativity
@@ -47,11 +48,11 @@ const char* ConvertDec(double decimalNumber, char** ptr, int base, int* length) 
 }
 
 // convert to dec from base 2 to 36
-double ConvertToDec(char* xBaseNumber, int base, int size) {
-  double output;
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; i < 36; j++) {
-      if (charTable[j] == xBaseNumber[i]) {
+int ConvertToDec(char* xBaseNumber, int base, int size) {
+  int output;
+  for (int i = 0; i < size; i++) { // nested for loops cuz thats how it is
+    for (int j = 0; i < 36; j++) { // really need to optimize this somehow
+      if (charTable[j] == xBaseNumber[i]) { // this is bad
         output += j*pow(base, size-i-1);
         break;
       }
@@ -61,7 +62,7 @@ double ConvertToDec(char* xBaseNumber, int base, int size) {
 }
 
 const char* ConvertBase(char** xBaseNumber, int base1, int base2, int* length) {
-  double decimalNumber = ConvertToDec(*xBaseNumber, base1, *length);
+  int decimalNumber = ConvertToDec(*xBaseNumber, base1, *length);
   ConvertDec(decimalNumber, xBaseNumber, base2, length);
 }
 
@@ -74,18 +75,18 @@ const char* HexToBin(char** hexadecimalNumber, int* length) {
   ConvertBase(hexadecimalNumber, 16, 2, length);
 }
 
-const char* DecToBin(double decimalNumber, char** ptr, int* length) {
+const char* DecToBin(int decimalNumber, char** ptr, int* length) {
   ConvertDec(decimalNumber, ptr, 2, length);
 }
 
-const char* DecToHex(double decimalNumber, char** ptr, int* length) {
+const char* DecToHex(int decimalNumber, char** ptr, int* length) {
   ConvertDec(decimalNumber, ptr, 16, length);
 }
 
-double BinToDec(char* binaryNumber, int length) {
+int BinToDec(char* binaryNumber, int length) {
   return ConvertToDec(binaryNumber, 2, length);
 }
 
-double HexToDec(char* hexadecimalNumber, int length) {
-  return ConvertToDec(hexadecimalNumber, 6, length);
+int HexToDec(char* hexadecimalNumber, int length) {
+  return ConvertToDec(hexadecimalNumber, 16, length);
 }
--- src/dbhc.c
+++ src/dbhc.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "../include/dbh.h"
 
-int main(int argc, char const *argv[]) {
+void PrinAll(char** outputString, int base, int*length);
+void PrintDecToAll(int decimalNumber, char** output, int* length);
+
+int main(int argc, char *argv[]) {
   // exemple use
-  double decimalNumber; // for xToDec functions
+  int decimalNumber; // for xToDec functions
   char* outputString; // for string outputs
   int length; // keep track of the output length
-  decimalNumber = 345;
-  printf("Dec:\t\t%lf\n", decimalNumber);
-  DecToBin(decimalNumber, &outputString, &length);
-  printf("Bin:\t\t%s\tLength:\t%d\n", outputString, length);
-  BinToHex(&outputString, &length);
-  printf("Hex:\t\t%s\t\tLength:\t%d\n", outputString, length);
-  ConvertBase(&outputString, 16, 12, &length);
-  printf("Base 12:\t%s\t\tLength:\t%d\n", outputString, length);
+  int base; // define number base
+
+  strcpy(outputString, argv[1]);
+  base = atoi(argv[2]);
+  length = strlen(outputString);
+
+  PrinAll(&outputString, base, &length);
+
   free(outputString); // allocated memory in function
   return 0;
+}
+
+void PrintDecToAll(int decimalNumber, char** output, int* length) {
+  for (int i = 2; i <= 36; i++) {
+    ConvertDec(decimalNumber, output, i, length);
+    if (i < 10) {
+      printf("Base 0%d:\t%s\n", i, *output); // to make it look better
+    } else {
+      printf("Base %d:\t%s\n", i, *output);
+    }
+  }
+}
+
+void PrinAll(char** outputString, int base, int*length) {
+  int decimalNumber = ConvertToDec(*outputString, base, *length);
+  PrintDecToAll(decimalNumber, outputString, length);
 }
