b328514b6bae57d5ce2fea32afab52e420dc72f5
agnitio247 <agnitio247@protonmail.com>
2019-04-15 23:48:54 -0400

    even number problem

--- src/dbhc.c
+++ src/dbhc.c
@@ -6,56 +6,56 @@
 #include "../include/dbh.h"
 
 #define MAX_BASE 36
-#define BUFFER 50
+#define BUFFER_SIZE 50
 
 void PrinAll(char** outputString, int base, int* length);
 void PrintDecToAll(int decimalNumber, char** output, int* length);
-char** ReadFile(char* filename, char*** outputArray, int* size, int* base);
+char* ReadFile(char* filename, char*** outputArray, int* size, int* base);
 
 int main(int argc, char *argv[]) {
+
+  int decimalNumber, size, length, base;
+  char* outputString;
+  char** outputArray;
+
+  if (ReadFile("input.txt", &outputArray, &size, &base) != "ERROR") {
+    printf("%s\n", "success");
+    printf("%s\n", outputArray[0]);
+    printf("%s\n", outputArray[1]);
+    printf("%s\n", outputArray[2]);
+    printf("%s\n", *(outputArray+3));
+    printf("%s\n", *(outputArray+4));
+    printf("%s\n", *(outputArray+5));
+    printf("%s\n", *(outputArray+6));
+    printf("%s\n", *(outputArray+7));
+  } else {
+    printf("%s\n", "Error opening file");
+  }
 
   return 0;
 }
 
-void PrintDecToAll(int decimalNumber, char** output, int* length) {
-  for (int i = 2; i <= MAX_BASE; i++) {
-    ConvertDec(decimalNumber, output, i, length);
-    if (i < 10) {
-      printf("Base 0%d:\t%s\n", i, *output); // to make it look better
-    } else {
-      printf("Base %d:\t%s\n", i, *output);
-    }
-  }
-}
-
-void PrinAll(char** outputString, int base, int* length) {
-  int decimalNumber = ConvertToDec(*outputString, base, *length);
-  PrintDecToAll(decimalNumber, outputString, length);
-}
-
-char** ReadFile(char* filename, char*** outputArray, int* size, int* base) {
-  char** output;
-  char buffer[BUFFER];
+char* ReadFile(char* filename, char*** outputArray, int* size, int* base) {
+  char buffer[BUFFER_SIZE];
   int maxLine, inputBase, currentLine, maxLength;
   FILE* file = fopen(filename, "r");
-  fscanf(file, "%d %d %d", &maxLine, &maxLength, &inputBase); // get file parameters
-  *size = maxLine;
-  *base = inputBase;
-  output = (char**)malloc(maxLine*sizeof(char)); // i know sizeof(char) == 1
-  for (int i = 0; i < maxLine; i++) { // allocate each string in array
-    output[i] = (char*)malloc(maxLength*sizeof(char) + 1);
-  }
-  currentLine = 0; // keep track of where we are
   if (file != NULL) { // NULL if file error
-    while (fscanf(file, "%s", buffer) == 1 && currentLine < maxLine) {
-      strcpy(output[currentLine], buffer);
-      strcat(output[currentLine], "\0");
+    fscanf(file, "%d %d %d", &maxLine, &maxLength, &inputBase); // get file parameters
+    char* output[maxLine]; // create output
+    currentLine = 0; // keep track of position
+    while (fscanf(file, "%s", buffer) == 1 && currentLine < maxLine) { // check errors
+      output[currentLine] = (char*)malloc(sizeof(char)*(maxLength+1)); //
+      strcpy(output[currentLine], strcat(buffer, "\0")); // need last char to be null
       currentLine++;
     }
+    *outputArray = output;
+    *size = maxLine;
+    *base = inputBase;
+    printf("%s\n", *((*outputArray)+3));
     fclose(file);
-  } else {
-    //return "FILE_ERROR";
+    return "SUCCESS";
+  } else { // if file not found
+    return "ERROR";
+    fclose(file);
   }
-  *outputArray = output;
-  return output;
 }
